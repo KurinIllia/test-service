c94fa0177967068507f34bb4da1b458a54f13c7f
Illia Kurin <ilku@ciklum.com>
2021-07-13 18:32:40 +0300

    small changes

--- src/test/java/com/kurininc/testservice/TestServiceApplicationTests.java
+++ src/test/java/com/kurininc/testservice/TestServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
