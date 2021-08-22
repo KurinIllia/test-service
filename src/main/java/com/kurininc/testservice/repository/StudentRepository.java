ca9c50563807bc21ab8a2ee34b5d00bf62707aeb
Illia Kurin <illia@mediarepublica.com>
2021-08-22 23:52:28 +0300

    Revert "add Redis"
    This reverts commit b3da5c869afe17e3d3cefd75fa866c3d81b7c354.

--- src/main/java/com/kurininc/testservice/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kurininc.testservice.repository;
-
-import com.kurininc.testservice.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, String> {
-}
