182498af8eb7eb144407468322fd7df00b958a37
Illia Kurin <illia@mediarepublica.com>
2021-08-22 23:52:34 +0300

    Revert "add Cassandra"
    This reverts commit c98453aa6f8df7537554747fd54214ee42b1fb25.

--- src/main/java/com/kurininc/testservice/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kurininc.testservice.repository;
-
-import com.kurininc.testservice.model.Person;
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CassandraRepository<Person, String> {
-}
